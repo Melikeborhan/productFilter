2395efae86cd6c4d07c07f76e9efd0fc8518b2ad
Melikeborhan <melikebrhnn@gmail.com>
2024-05-23 14:47:13 +0300

    fixed position editing

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
     <body className="bg-gray-100">
-      <div className="fixed top-0 left-0 right-0 h-16 z-50 bg-white border-b border-gray-300">
+      <div className="fixed top-0 left-0  right-0 h-16 z-50 bg-white border-b border-gray-300">
         <Navbar className="w-full h-full" />
       </div>
       <div className="flex pt-16 lg:pt-14  lg:mb-20">
-        <div className="fixed lg:top-16 bottom-16 left-0 max-h-screen lg:w-1/6 w-full bg-white border-r border-gray-300">
+        <div className="fixed lg:top-16 mt-12 bottom-16 left-0 max-h-screen lg:w-1/6 w-full bg-white border-r border-gray-300">
           <Sidebar className="h-full" />
         </div>
         <div className="lg:ml-60 mt-16 lg:mt-0 lg:w-5/6 lg:p-4 w-full px-8">
